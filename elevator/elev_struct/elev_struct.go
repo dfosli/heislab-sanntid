122889da1448ecde259c516f37faa1d456d5051c
Tobias Hellerslien <services@tobias.hellerslien.net>
2026-02-11 17:28:49 +0100

    made basic structs

--- elevator/elev_struct/elev_struct.go
+++ elevator/elev_struct/elev_struct.go
@@ -1,13 +1,13 @@
-package elevstruct
+package elev_struct
 
 import (
-	"heislab-sanntid/elevator/elevio"
 	"heislab-sanntid/config"
+	"heislab-sanntid/elevator/elevio"
 )
 
 const (
 	N_FLOORS  int = config.N_FLOORS
-	N_BUTTONS 	  = config.N_BUTTONS
+	N_BUTTONS int = config.N_BUTTONS
 )
 
 type State int
@@ -18,3 +18,26 @@ const (
 	DoorOpen
 )
 
+type Orders [N_FLOORS][N_BUTTONS]bool
+
+type Elevator struct {
+	State  State
+	Floor  int
+	Dir    elevio.MotorDirection
+	Orders Orders
+	ID     int
+}
+
+func ElevatorInit(id int) Elevator {
+	return Elevator{
+		State: Idle,
+		Floor: -1,
+		Dir:   elevio.MD_Stop,
+		ID:    id,
+	}
+}
+
+type DirStatePair struct {
+	Dir   elevio.MotorDirection
+	State State
+}
