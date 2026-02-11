e5026527934d4a908e399666ea5acd625f77a2a9
Tobias Hellerslien <services@tobias.hellerslien.net>
2026-02-11 17:29:01 +0100

    started on requests

--- elevator/requests/requests.go
+++ elevator/requests/requests.go
@@ -1,2 +1,47 @@
 package requests
 
+import (
+	"heislab-sanntid/elevator/elevio"
+	"heislab-sanntid/elevator/elev_struct"
+	"heislab-sanntid/config"
+)
+
+const (
+	N_FLOORS  int = config.N_FLOORS
+	N_BUTTONS int = config.N_BUTTONS
+)
+
+
+
+""" c kode
+static int requests_above(Elevator e){
+    for(int f = e.floor+1; f < N_FLOORS; f++){
+        for(int btn = 0; btn < N_BUTTONS; btn++){
+            if(e.requests[f][btn]){
+                return 1;
+            }
+        }
+    }
+    return 0;
+}
+
+static int requests_below(Elevator e){
+    for(int f = 0; f < e.floor; f++){
+        for(int btn = 0; btn < N_BUTTONS; btn++){
+            if(e.requests[f][btn]){
+                return 1;
+            }
+        }
+    }
+    return 0;
+}
+
+static int requests_here(Elevator e){
+    for(int btn = 0; btn < N_BUTTONS; btn++){
+        if(e.requests[e.floor][btn]){
+            return 1;
+        }
+    }
+    return 0;
+}
+"""
